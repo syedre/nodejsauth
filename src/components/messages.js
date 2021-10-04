cea7483d759faef155beb5199c47b1b2bdb3495c
syedre <syed.rehan123@gmail.com>
2021-10-04 12:06:03 +0530

    new feature

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,10 +1,13 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import './chat.css';
 import ScrollToBottom from 'react-scroll-to-bottom';
+// import SendIcon from '@mui/icons-material/Send';
+import SendIcon from '@material-ui/icons/Send';
 
 function Messages({socket,username,room}) {
     const [currentMessage,setcurrentmessage] = useState(''); 
-    const [messageList,setmessageList] = useState([]); 
+    const [messageList,setmessageList] = useState([]);
+    const focusEmpty = useRef(null); 
 
     const sendMessage = async() => {
         if(currentMessage!==''){
@@ -17,6 +20,7 @@ function Messages({socket,username,room}) {
             }
             await socket.emit("send_message",messageData);
             setmessageList((list)=>[...list,messageData]);
+            focusEmpty.current.value=""; 
             
 
 
@@ -27,10 +31,9 @@ function Messages({socket,username,room}) {
        
        socket.on("recieve_message",(data)=>{
            setmessageList((list)=>[...list,data]);
-          
-
-       })
+        })
     }, [socket])
+    
      return (
         <div className="chat-window">
            <div className="chat-header">
@@ -61,10 +64,13 @@ function Messages({socket,username,room}) {
            </div>
            <div className="chat-footer">
                <input type="text"
+                ref={focusEmpty} 
                onChange={(event)=>{setcurrentmessage(event.target.value)}}
                 placeholder="hey......"/>
+               
                <button onClick={sendMessage}>
-                    entr
+                   <SendIcon/>
+                   {/* <AccessAlarmIcon /> */}
                </button>
            </div>
             
